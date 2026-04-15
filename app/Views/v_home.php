f2efcbbca2cf5aa2992dfc9a87eb870a66f3c4f5
Huga Hazimulfikri <huga36360@gmail.com>
2026-04-15 14:41:49 +0700

    feat: menyelesaikan tugas capstone 3 layout CI4

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -1,4 +1,8 @@
-ini halaman home<br>
-<a href="/produk">ke halaman produk</a><br>
-<a href="/keranjang">ke halaman keranjang</a>
+<?= $this->extend('layout/template'); ?>
 
+<?= $this->section('content'); ?>
+
+    <h2>Selamat Datang di Halaman Home!</h2>
+    <p>Ini adalah bukti bahwa fitur View Layouts di CodeIgniter 4 sudah berhasil diterapkan. Bagian header dan footer berasal dari file template terpisah.</p>
+
+<?= $this->endSection(); ?>
